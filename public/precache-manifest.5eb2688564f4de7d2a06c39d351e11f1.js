d6b8c6906638935177c01726b27523894356384a
Ammar Faizi <ammarfaizi2@gmail.com>
2019-09-21 22:37:16 +0700

    Initial commit

---
+++ public/precache-manifest.5eb2688564f4de7d2a06c39d351e11f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b2c967354d0b13f5e4cc2ee6141696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cba33159e1083b26275",
+    "url": "/static/css/main.79673ba8.chunk.css"
+  },
+  {
+    "revision": "8d0062e1bb5cb34206ac",
+    "url": "/static/js/2.6f93bd41.chunk.js"
+  },
+  {
+    "revision": "4cba33159e1083b26275",
+    "url": "/static/js/main.7839b7ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/static/media/reactjs.47ce6e77.png"
+  }
+]);
